a103c62d209329d4d97b3ed1196486334ebebb32
Michael Prokofyev <mikeprokofyev@gmail.com>
2017-06-01 20:51:25 +0300

    Position Reset on touchpad touch added

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : SingletonComponent<GameController> {
+
+    public Transform cameraRig;
+
+    public Transform mainCamera;
+
+    public Transform teleportLocation;
+    
+    public void ResetPlayerPosition () {
+        Vector3 startRigPostion = teleportLocation.position - mainCamera.localPosition;
+        cameraRig.position = new Vector3(startRigPostion.x, 0, startRigPostion.z);
+    }
+
+	// Use this for initialization
+	void Start () {
+        ResetPlayerPosition();
+    }
+	
+	void Update () {
+
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class PlayerController : MonoBehaviour {
+
+    public VRTK_ControllerEvents rightController;
+    // Use this for initialization
+    void Start () {
+        rightController.TouchpadPressed += ResetPosition;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        print("HELL YE");
+    }
+
+    void ResetPosition (object sender, ControllerInteractionEventArgs args)
+    {
+        GameController.Instance.ResetPlayerPosition();
+    }
+}
