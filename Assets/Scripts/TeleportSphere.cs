2aff001523553adb531d0f52c97762ceef8d8ef5
Michael Prokofyev <mikeprokofyev@gmail.com>
2017-06-02 16:22:54 +0300

    main scene updated

---
+++ Assets/Scripts/TeleportSphere.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+public class TeleportSphere : VRTK_InteractableObject {
+
+    Vector3 startPosition;
+
+    bool returningToStart;
+
+	// Use this for initialization
+	void Start () {
+        startPosition = transform.position;
+        InteractableObjectUngrabbed += ReturnToStart;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void ReturnToStart(object sender, InteractableObjectEventArgs e) {
+    //    returningToStart = true;
+        transform.position = startPosition;
+    }
+}
