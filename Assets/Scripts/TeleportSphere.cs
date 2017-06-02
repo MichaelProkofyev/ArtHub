2d05a8543a687818e3f2118b7645d7dc046a3f08
Michael Prokofyev <mikeprokofyev@gmail.com>
2017-06-02 18:24:50 +0300

    teleport sphere updated

--- Assets/Scripts/TeleportSphere.cs
+++ Assets/Scripts/TeleportSphere.cs
@@ -4,32 +4,61 @@ using UnityEngine;
 using VRTK;
 public class TeleportSphere : VRTK_InteractableObject
 {
-    [SerializeField]
-    private string sceneName;
 
     Vector3 startPosition;
 
-    bool returningToStart;
+    public bool returningToStart;
+    public bool dragged;
+    Rigidbody rb;
 
     // Use this for initialization
     void Start()
     {
         startPosition = transform.position;
         InteractableObjectUngrabbed += ReturnToStart;
+        InteractableObjectGrabbed += MakeGrabbable;
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void MakeGrabbable (object sender, InteractableObjectEventArgs e)
     {
+        dragged = true;
+    }
 
+
+    // Update is called once per frame
+    override protected void FixedUpdate()
+    {
+        Debug.Log(rb.velocity);
+        base.FixedUpdate();
+        if (returningToStart)
+        {
+            float distanceToStart = Vector3.Distance(startPosition, transform.position);
+            Vector3 force = (startPosition - transform.position).normalized * 100f;
+            rb.AddForce(force);
+
+            // rb.AddRelativeForce(direction.normalized * speed, ForceMode.Force);
+
+            if (distanceToStart <= 0.5)
+            {
+                //
+                returningToStart = false;
+                print("STOPPING");
+                transform.position = startPosition;
+               
+            }
+        }
+        else if(!dragged)
+        {
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+        }
     }
 
     void ReturnToStart(object sender, InteractableObjectEventArgs e)
     {
-        //    returningToStart = true;
-        transform.position = startPosition;
+        returningToStart = true;
+        dragged = false;
+        //transform.position = startPosition;
     }
-
-    public string GetSceneName()
-    { return sceneName; }
 }
